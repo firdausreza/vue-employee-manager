1f5a73959a4aa72af604c25ca59a74dba09bf005
Reza Azerly <kiritosao080@gmail.com>
2021-10-08 14:51:05 +0700

    Adding Create Employee Feature

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -8,7 +8,7 @@
         <div class="chip">
           {{ employee.dept }}
         </div>
-        {{ employee.employee_id }} : {{ employee.name }}
+        {{ employee.employee_id }} - {{ employee.name }} - {{ employee.position }}
       </li>
     </ul>
 
@@ -31,22 +31,27 @@ export default {
     }
   },
   created() {
-    const db = firebase.firestore();
-    db.collection('employees').get().then(querySnapshot => {
-      querySnapshot.forEach(doc => {
-        const data = {
-          'id': doc.id,
-          'employee_id': doc.data().employee_id,
-          'name': doc.data().name,
-          'dept': doc.data().dept,
-          'position': doc.data().position
-        }
-        this.employees.push(data);
+    this.getEmployees();
+  },
+  methods: {
+    getEmployees() {
+      const db = firebase.firestore();
+      db.collection('employees').get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          const data = {
+            'id': doc.id,
+            'employee_id': doc.data().employee_id,
+            'name': doc.data().name,
+            'dept': doc.data().dept,
+            'position': doc.data().position
+          }
+          this.employees.push(data);
+        })
       })
-    })
-    .catch(err => {
-      alert(err.message);
-    })
+      .catch(err => {
+        alert(err.message);
+      })
+    }
   }
 }
 </script>
--- src/components/NewEmployee.vue
+++ src/components/NewEmployee.vue
@@ -1,12 +1,72 @@
 <template>
   <div id="new-employee">
     <h3>Add New Employee</h3>
+
+    <div class="row">
+      <form class="col s12" @submit="setEmployee">
+        <div class="row">
+          <div class="row">
+            <div class="col s4">
+              <div class="input-field">
+                <input id="name_inline" type="text" class="validate" v-model="name">
+                <label for="name_inline">Full Name</label>
+              </div>
+            </div>
+            <div class="col s4">
+              <div class="input-field">
+                <input id="dept_inline" type="text" class="validate" v-model="dept">
+                <label for="dept_inline">Department</label>
+              </div>
+            </div>
+            <div class="col s4">
+              <div class="input-field">
+                <input id="pos_inline" type="text" class="validate" v-model="position">
+                <label for="pos_inline">Position</label>
+              </div>
+            </div>
+          </div>
+          <button class="btn waves-effect waves-light block" type="submit" style="display: block; width: 100%">
+            Submit
+          </button>
+        </div>
+      </form>
+      <p>{{ name }} {{ dept }} {{ position }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import firebase from "./firebaseConfig";
+
 export default {
-  name: "new-employee"
+  name: "new-employee",
+  data() {
+    return {
+      name: '',
+      dept: '',
+      position: '',
+      id: 0,
+    }
+  },
+  created() {
+    const db = firebase.firestore();
+    db.collection('employees').get().then(querySnapshot => {
+      this.id = querySnapshot.docs.length
+    })
+  },
+  methods: {
+    setEmployee() {
+      const db = firebase.firestore();
+      const employee = {
+        employee_id: this.id + 1,
+        name: this.name,
+        dept: this.dept,
+        position: this.position
+      }
+      db.collection('employees').add(employee).then(() => alert('Added new employee!')).catch(err => alert('Failed to add new employee'))
+      this.$router.push("/");
+    }
+  }
 }
 </script>
 
