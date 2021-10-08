31a3a4ce2931e8fceb76be160c594cfafd9b963a
Reza Azerly <kiritosao080@gmail.com>
2021-10-08 12:56:59 +0700

    Initial firebase connection to vue app

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="dashboard">
-    <h3>Dashboard</h3>
+    <ul class="collection with-header">
+      <li class="collection-header">
+        <h4>Employees</h4>
+      </li>
+      <li v-for="employee in employees" :key="employee.id" class="collection-item">
+        <div class="chip">
+          {{ employee.dept }}
+        </div>
+        {{ employee.employee_id }} : {{ employee.name }}
+      </li>
+    </ul>
 
     <div class="fixed-action-btn">
       <router-link to="/new" class="btn-floating btn-large red">
@@ -11,12 +21,32 @@
 </template>
 
 <script>
+import firebase from "./firebaseConfig";
+
 export default {
   name: "Dashboard",
   data() {
     return {
-
+      employees: []
     }
+  },
+  created() {
+    const db = firebase.firestore();
+    db.collection('employees').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        const data = {
+          'id': doc.id,
+          'employee_id': doc.data().employee_id,
+          'name': doc.data().name,
+          'dept': doc.data().dept,
+          'position': doc.data().position
+        }
+        this.employees.push(data);
+      })
+    })
+    .catch(err => {
+      alert(err.message);
+    })
   }
 }
 </script>
--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,4 +1,6 @@
-export default {
+import firebase from "firebase/compat";
+
+const firebaseConfig = {
     apiKey: "AIzaSyC5xZE9b63wMZn9pnK2eeHnQWgrh_CSzMg",
     authDomain: "vuefs-prod-5b634.firebaseapp.com",
     projectId: "vuefs-prod-5b634",
@@ -6,3 +8,5 @@ export default {
     messagingSenderId: "6794414423",
     appId: "1:6794414423:web:ea66c959751596286b9612"
 }
+
+export default firebase.initializeApp(firebaseConfig)
